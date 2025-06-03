7e964de5c4ae90e3fa8bcfbdb805d0c406124553
Strawberry0831 <2015032010@bcis.cn>
2025-06-03 09:46:00 +0800

    Add files via upload

---
+++ Welcome_page.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.balloons()
+st.subheader("🌷:rainbow[Welcome!!!]✨")
+st.divider()
+st.write("On this online learning platform, you will be able to access some basic biology information and content that is intended for elementary and middle school students. With this website, we hope to help you explore simple topics of biology and develop interest in them. Have fun!⭐️")
+st.divider()
+st.write("Biology")
+st.text("Let's start with the definition of biology as a subject🤩")
+st.text("Biology is defined as a natural science discipline that studies living things. It is truly a large field, and many scientists only focus on 1 specific field. Many only study 1 type of organism/living being.🍀 It is a big branch of science. Basically, it's the study of life🌸!!!")
+st.text("Citation:Rogers, Kara, and Susan Heyner Joshi. “Biology.” Encyclopædia Britannica, 30 Nov. 2018, www.britannica.com/science/biology.")
+st.page_link("Content.py", label="Overview", icon="✨")
